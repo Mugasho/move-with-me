cb5053fb2192c266934776f1a69ffc2231db5e68
Mugasho Lincoln <mugalink2@gmail.com>
2017-11-24 12:12:21 +0300

    Login changed to move with me

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -70,13 +70,13 @@
          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
        </li>
        <li class="nav-item">
-         <a class="nav-link" href="#">Features</a>
+         <a class="nav-link" href="#">Groups</a>
        </li>
        <li class="nav-item">
-         <a class="nav-link" href="#">Pricing</a>
+         <a class="nav-link" href="#">Attractions</a>
        </li>
        <li class="nav-item">
-         <a class="nav-link disabled" href="#">Disabled</a>
+         <a class="nav-link" href="#">Accomodation</a>
        </li>
      </ul>
    </div>
